3e440f8eb824cbf47b3aa10d1bddb8fe0c83f494
PriyankaPawar47 <priyankapwr47@gmail.com>
2018-08-19 07:23:15 -0700

    19/08

--- file1.R
+++ file1.R
@@ -2,3 +2,10 @@ mtcars
 git config --global user.email "priyankapwr47@gmail.com"
 git config --global user.name "PriyankaPawar47"
 women
+plot(women)
+?women
+?mean
+x=1:100
+x
+y=seq(1,100, by=2)
+install.packages('car')
